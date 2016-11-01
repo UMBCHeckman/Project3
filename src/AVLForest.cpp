0e29e63c33280d1e1ef037b47249e4a058f34900
SATYR\kithe_000 <heckman1@umbc.edu>
2016-10-31 21:09:56 -0400

    okay

--- src/AVLForest.cpp
+++ src/AVLForest.cpp
@@ -29,38 +29,41 @@ void AVLForest::insert(std::string line)
   if((m_forestMap.find( base[0] )) == m_forestMap.end())/* check if tree not already in vector */
   {
     // create a new tree and add it to the vector
-      int temp = atoi(base[1]);
       AVLTreeBase::PrintOrder tempOrder;
-      tempOrder = static_cast<AVLTreeBase::DataType>(base[1]);
+      tempOrder = static_cast<AVLTreeBase::PrintOrder>(atoi(base[1].c_str()));
       AVLTreeBase::DataType tempData;
-      tempData = static_cast<AVLTreeBase::DataType>(base[2]);
+      tempData = static_cast<AVLTreeBase::DataType>(atoi(base[2].c_str()));
       AVLTreeBase::NumberType tempNumber;
-      tempNumber = static_cast<AVLTreeBase::DataType>(base[3]);
+      tempNumber = static_cast<AVLTreeBase::NumberType>(atoi(base[3].c_str()));
       //tree *myTreeBase = new tree;
-      myTreeBase->PrintOrder(base[1]);
-      myTreeBase->DataType(base[2]);
-      myTreeBase->NumberType(base[3]);
-      myTrees.push_back(myTreeBase);
-      m_forestMap.insert ( std::pair<std::string,AVLTreeBase*>(base[0],myTreeBase) );
+//      myTreeBase->PrintOrder(base[1]);
+//      myTreeBase->DataType(base[2]);
+//      myTreeBase->NumberType(base[3]);
+//      myTrees.push_back(myTreeBase);
+//      m_forestMap.insert ( std::pair<std::string,AVLTreeBase*>(base[0],myTreeBase) );
       //AVLTreeBase* b = new tree;
       //my tree has been created but not static casted yet
 
     if((tempData == 0) && (tempNumber == 0))/* character and integer */
     {
-        //tree *myTreeBase = new tree;
-        static_cast<tree<char, int>*>
+        AVLTreeBase *myBase;
+        tree<char, int> *myTreeBase = new tree<char, int>();
+        //myBase->PrintOrder = tempOrder; myBase->DataType = tempData; myBase->NumberType = tempNumber;
+        myTreeBase = static_cast<tree<char, int>*>(myBase);
+        myTreeBase->setPrintOrder(tempOrder); myTreeBase->setDataType(tempData); myTreeBase->setNumberType(tempNumber);
+        myTrees.push_back(myTreeBase);
       //static_cast<myTrees<char, int>*>/* static_cast to <YourAVLTreeClassName<char, int>*> if needed */
     }
-    else if(/* character and float */)
+    else if((tempData == 0) && (tempNumber == 1))/* character and float */
     {
     /* static_cast to <YourAVLTreeClassName<char, float>*> if needed */
 
     }
-    else if(/* string and integer */)
+    else if((tempData == 1) && (tempNumber == 0))/* string and integer */
     {
       /* static_cast to <YourAVLTreeClassName<std::string, int>*> if needed */
     }
-    else if((/* string and float */))
+    else if((tempData == 0) && (tempNumber == 0))/* string and float */
     {
       /* static_cast to <YourAVLTreeClassName<std::string, float>*> if needed */
     }
@@ -73,12 +76,20 @@ void AVLForest::insert(std::string line)
   {
     // tree is in vector
     // we can proceed with insert
-
+      AVLTreeBase::DataType tempData;
+      tempData = static_cast<AVLTreeBase::DataType>(atoi(base[1].c_str()));
+      AVLTreeBase::NumberType tempNumber;
+      tempNumber = static_cast<AVLTreeBase::NumberType>(atoi(base[2].c_str()));
     // insert data, number pair
 
-    if(/* character and integer */)
+    if((tempData == 0) && (tempNumber == 0))//* character and integer */)
     {
-      /* static_cast to <YourAVLTreeClassName<char, int>*> if needed */
+      //tree<char, int> *myTreeBase = static_cast<tree<char, int>*>(myBase);/* static_cast to <YourAVLTreeClassName<char, int>*> if needed */
+      AVLTreeBase *myBase;
+      tree<char, int> *myTreeBase = static_cast<tree<char, int>*>(myBase);
+      AVLTreeBase *myTree = m_forestMap.find(base[0])->second;
+      myTree->setPrintOrder(tempOrder); myTree->setDataType(tempData); myTree->setNumberType(tempNumber);
+
     }
     else if(/* character and float */)
     {
